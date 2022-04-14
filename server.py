1115594e8c80302610d676f495c4ef293b84f19f
marouaJerbi <36313623+marouaJerbi@users.noreply.github.com>
2022-04-14 16:59:35 -0400

    juste for test

--- server.py
+++ server.py
@@ -1,20 +1,9 @@
-'''
-    Contains the server to run our application.
-'''
-from flask_failsafe import failsafe
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return 'hello world'
 
-@failsafe
-def create_app():
-    '''
-        Gets the underlying Flask server from our Dash app.
-        Returns:
-            The server to be run
-    '''
-    # the import is intentionally inside to work with the server failsafe
-    from app import app  # pylint: disable=import-outside-toplevel
-    return app.server
-
-
-if __name__ == "__main__":
-    create_app().run()
+if __name__ == '__main__':
+    app.run()
